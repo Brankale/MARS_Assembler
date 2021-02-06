2f24b83086033c555d343f7d3da33eba18aca081
Brankale <45881310+Brankale@users.noreply.github.com>
2021-02-06 15:35:12 +0100

    simplified tools loading logic
    The old logic relied on the path of the classes which was a bit odd. This will help re-organising classes in different packages without breaking everything.

--- src/main/java/mars/venus/ToolLoader.java
+++ src/main/java/mars/venus/ToolLoader.java
@@ -1,15 +1,13 @@
 package mars.venus;
 
 import mars.tools.*;
-import mars.util.*;
 import mars.venus.actions.ToolAction;
 
 import javax.swing.*;
 import java.awt.event.*;
 import java.util.*;
-import java.lang.reflect.*;
 	
-	/*
+/*
 Copyright (c) 2003-2006,  Pete Sanderson and Kenneth Vollmar
 
 Developed by Pete Sanderson (psanderson@otterbein.edu)
@@ -42,112 +40,74 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * system by adding them to its Tools menu.  This permits anyone with knowledge
  * of the Mars public interfaces, in particular of the Memory and Register
  * classes, to write applications which can interact with a MIPS program
- * executing under Mars.  The execution is of course simulated.  The
- * private method for loading tool classes is adapted from Bret Barker's
- * GameServer class from the book "Developing Games In Java".
- *
- * @author Pete Sanderson with help from Bret Barker
- * @version August 2005
+ * executing under Mars.  The execution is of course simulated.
  */
-
 public class ToolLoader {
 
-    private static final String CLASS_PREFIX = "mars.tools.";
-    private static final String TOOLS_DIRECTORY_PATH = "mars/tools";
     private static final String TOOLS_MENU_NAME = "Tools";
-    private static final String MARSTOOL_INTERFACE = "MarsTool.class";
-    private static final String CLASS_EXTENSION = "class";
 
     /**
-     * Called in VenusUI to build its Tools menu.  If there are no qualifying tools
-     * or any problems accessing those tools, it returns null.  A qualifying tool
-     * must be a class in the Tools package that implements MarsTool, must be compiled
-     * into a .class file, and its .class file must be in the same Tools folder as
-     * MarsTool.class.
-     *
-     * @return a Tools JMenu if qualifying tool classes are found, otherwise null
+     * Called in VenusUI to build its Tools menu.
+     * @return a JMenu if there are tools available, null otherwise
      */
     public JMenu buildToolsMenu() {
-        JMenu menu = null;
-        ArrayList<MarsToolClassAndInstance> marsToolList = loadMarsTools();
-        if (!marsToolList.isEmpty()) {
-            menu = new JMenu(TOOLS_MENU_NAME);
+        ArrayList<MarsToolClassAndInstance> tools = loadTools();
+
+        if (!tools.isEmpty()) {
+            JMenu menu = new JMenu(TOOLS_MENU_NAME);
             menu.setMnemonic(KeyEvent.VK_T);
-            // traverse array list and build menu
-            MarsToolClassAndInstance listItem;
-            for (MarsToolClassAndInstance marsToolClassAndInstance : marsToolList) {
-                listItem = marsToolClassAndInstance;
-                menu.add(new ToolAction(listItem.marsToolClass, listItem.marsToolInstance.getName()));
+            for (MarsToolClassAndInstance tool : tools) {
+                ToolAction toolAction = new ToolAction(tool.getToolClass(), tool.getToolName());
+                menu.add(toolAction);
             }
+            return menu;
+        } else {
+            return null;
         }
-        return menu;
     }
 
-    /*
-     *  Dynamically loads MarsTools into an ArrayList.  This method is adapted from
-     *  the loadGameControllers() method in Bret Barker's GameServer class.
-     *  Barker (bret@hypefiend.com) is co-author of the book "Developing Games
-     *  in Java".  It was demo'ed to me by Otterbein student Chris Dieterle
-     *  as part of his Spring 2005 independent study of implementing a networked
-     *  multi-player game playing system.  Thanks Bret and Chris!
-     *
-     *  Bug Fix 25 Feb 06, DPS: method did not recognize tools folder if its
-     *  absolute pathname contained one or more spaces (e.g. C:\Program Files\mars\tools).
-     *  Problem was, class loader's getResource method returns a URL, in which spaces
-     *  are replaced with "%20".  So I added a replaceAll() to change them back.
-     *
-     *  Enhanced 3 Oct 06, DPS: method did not work if running MARS from a JAR file.
-     *  The array of files returned is null, but the File object contains the name
-     *  of the JAR file (using toString, not getName).  Extract that name, open it
-     *  as a ZipFile, get the ZipEntry enumeration, find the class files in the tools
-     *  folder, then continue as before.
+    /**
+     * To add a new Mars tool just add a new MarsToolClassAndInstance entry to the tools list.
+     * @return a list of the available tools
      */
-    private ArrayList<MarsToolClassAndInstance> loadMarsTools() {
-        ArrayList<MarsToolClassAndInstance> toolList = new ArrayList<>();
-        ArrayList<String> candidates = FilenameFinder.getFilenameList(this.getClass().getClassLoader(),
-                TOOLS_DIRECTORY_PATH, CLASS_EXTENSION);
-        // Add any tools stored externally, as listed in Config.properties file.
-        // This needs some work, because mars.Globals.getExternalTools() returns
-        // whatever is in the properties file entry.  Since the class file will
-        // not be located in the mars.tools folder, the loop below will not process
-        // it correctly.  Not sure how to create a Class object given an absolute
-        // pathname.
-        //candidates.addAll(mars.Globals.getExternalTools());  // this by itself is not enough...
-        HashMap<String, String> tools = new HashMap<>();
-        for (String file : candidates) {
-            // Do not add class if already encountered (happens if run in MARS development directory)
-            if (tools.containsKey(file)) {
-                continue;
-            } else {
-                tools.put(file, file);
-            }
-            if (!file.equals(MARSTOOL_INTERFACE)) {
-                try {
-                    // grab the class, make sure it implements MarsTool, instantiate, add to menu
-                    String toolClassName = CLASS_PREFIX + file.substring(0, file.indexOf(CLASS_EXTENSION) - 1);
-                    Class clas = Class.forName(toolClassName);
-                    if (!MarsTool.class.isAssignableFrom(clas) ||
-                            Modifier.isAbstract(clas.getModifiers()) ||
-                            Modifier.isInterface(clas.getModifiers())) {
-                        continue;
-                    }
-                    toolList.add(new MarsToolClassAndInstance(clas, (MarsTool) clas.newInstance()));
-                } catch (Exception e) {
-                    System.out.println("Error instantiating MarsTool from file " + file + ": " + e);
-                }
-            }
-        }
-        return toolList;
+    private ArrayList<MarsToolClassAndInstance> loadTools() {
+        // TODO: the instance of the class is needed just to retrieve the title to
+        //       display in the "Tools" menu. I don't like the idea of writing the
+        //       title here since it should be declared in the corresponding class.
+        //       Find a way to retrieve the title without instantiating the class.
+        ArrayList<MarsToolClassAndInstance> tools = new ArrayList<>();
+        tools.add(new MarsToolClassAndInstance(IntroToTools.class, new IntroToTools()));
+        tools.add(new MarsToolClassAndInstance(BHTSimulator.class, new BHTSimulator()));
+        tools.add(new MarsToolClassAndInstance(BitmapDisplay.class, new BitmapDisplay()));
+        tools.add(new MarsToolClassAndInstance(CacheSimulator.class, new CacheSimulator()));
+        tools.add(new MarsToolClassAndInstance(DigitalLabSim.class, new DigitalLabSim()));
+        tools.add(new MarsToolClassAndInstance(FloatRepresentation.class, new FloatRepresentation()));
+        tools.add(new MarsToolClassAndInstance(InstructionCounter.class, new InstructionCounter()));
+        tools.add(new MarsToolClassAndInstance(InstructionStatistics.class, new InstructionStatistics()));
+        tools.add(new MarsToolClassAndInstance(KeyboardAndDisplaySimulator.class, new KeyboardAndDisplaySimulator()));
+        tools.add(new MarsToolClassAndInstance(MarsBot.class, new MarsBot()));
+        tools.add(new MarsToolClassAndInstance(MemoryReferenceVisualization.class, new MemoryReferenceVisualization()));
+        tools.add(new MarsToolClassAndInstance(MipsXray.class, new MipsXray()));
+        tools.add(new MarsToolClassAndInstance(ScavengerHunt.class, new ScavengerHunt()));
+        tools.add(new MarsToolClassAndInstance(ScreenMagnifier.class, new ScreenMagnifier()));
+        return tools;
     }
 
+    private static class MarsToolClassAndInstance {
+        private final Class<?> marsToolClass;
+        private final MarsTool marsToolInstance;
 
-    private class MarsToolClassAndInstance {
-        Class marsToolClass;
-        MarsTool marsToolInstance;
-
-        MarsToolClassAndInstance(Class marsToolClass, MarsTool marsToolInstance) {
+        MarsToolClassAndInstance(Class<?> marsToolClass, MarsTool marsToolInstance) {
             this.marsToolClass = marsToolClass;
             this.marsToolInstance = marsToolInstance;
+        }
+
+        public Class<?> getToolClass() {
+            return marsToolClass;
+        }
+
+        public String getToolName() {
+            return marsToolInstance.getName();
         }
     }
 }
